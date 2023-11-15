ce289db3f67b93b36d6ceeb1985a8b4c225fdba0
Afriyaniwulandari <yaniwulandari2022@gmail.com>
2023-11-15 10:57:04 +0700

    Perbaikan

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER' : 'root',
+        'PASSWORD' : 'dianafri23',
+        'PORT' : '3306',
+        'HOST' : 'localhost',
     }
 }
 
